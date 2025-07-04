b9044cb04f359659853948aa6238b1ff0b0108f8
Bartek Dygasiński <bartlomiej.dygasinski@tomtom.com>
2025-07-04 11:38:36 +0200

    Test Entry static factory method.

---
+++ src/test/java/pl/bdygasinski/filewalker/EntryTest.java
@@ -0,0 +1,66 @@
+package pl.bdygasinski.filewalker;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchException;
+import static org.junit.jupiter.api.Assertions.*;
+import static pl.bdygasinski.filewalker.ClassLoadingUtil.ROOT_DIR;
+import static pl.bdygasinski.filewalker.ClassLoadingUtil.classpathResource;
+
+@DisplayName("Entry unit tests")
+class EntryTest {
+
+
+    @DisplayName("FromPathOrThrow unit tests")
+    @Nested
+    class FormPathOrThrowTest {
+
+        @DisplayName("Should produce FileEntry if path refers to file")
+        @Test
+        void should_return_file_entry() {
+            // Given
+            Path givenPath = Path.of(classpathResource(ROOT_DIR + "/2/1.txt").orElseThrow());
+
+            // When
+            Entry result = Entry.fromPathOrThrow(givenPath);
+
+            // Then
+            assertThat(result)
+                    .isExactlyInstanceOf(FileEntry.class);
+        }
+
+        @DisplayName("Should produce DirEntry if path refers to dir")
+        @Test
+        void should_return_dir_entry() {
+            // Given
+            Path givenPath = Path.of(classpathResource(ROOT_DIR).orElseThrow());
+
+            // When
+            Entry result = Entry.fromPathOrThrow(givenPath);
+
+            // Then
+            assertThat(result)
+                    .isExactlyInstanceOf(DirEntry.class);
+        }
+
+        @DisplayName("Should throw IllegalArgumentException when can't produce file or dir entry")
+        @Test
+        void should_throw() {
+            // Given
+            Path givenPath = null;
+
+            // When
+            Exception result = catchException(() -> Entry.fromPathOrThrow(givenPath));
+
+            // Then
+            assertThat(result)
+                    .isExactlyInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+}
