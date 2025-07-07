41246984372ec265dde0f6fcce06712ca9821197
Bartek Dygasiński <bartlomiej.dygasinski@tomtom.com>
2025-07-07 13:08:02 +0200

    Moved small implementation files to same files with API.

--- src/main/java/pl/bdygasinski/filewalker/ContentProvider.java
+++ src/main/java/pl/bdygasinski/filewalker/ContentProvider.java
@@ -1,5 +1,7 @@
 package pl.bdygasinski.filewalker;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.Path;
 import java.util.Set;
 
@@ -10,4 +12,18 @@ public interface ContentProvider {
     }
 
     Set<Entry> provideEntriesFrom(Path path);
+}
+
+class ConsoleContentProvider implements ContentProvider{
+
+    @Override
+    public Set<Entry> provideEntriesFrom(Path path) {
+        try {
+            Entry rootEntry = Entry.fromPathOrThrow(path);
+            return rootEntry.getVisibleRootLevelOrThrow();
+
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
 }
--- src/main/java/pl/bdygasinski/filewalker/ContentVisualizer.java
+++ src/main/java/pl/bdygasinski/filewalker/ContentVisualizer.java
@@ -1,6 +1,7 @@
 package pl.bdygasinski.filewalker;
 
 import java.nio.file.Path;
+import java.util.Set;
 
 import static java.util.Objects.requireNonNull;
 
@@ -12,5 +13,22 @@ public interface ContentVisualizer {
 
     static ContentVisualizer getInstance(ContentProvider contentProvider) {
         return new ConsoleContentVisualizer(requireNonNull(contentProvider));
+    }
+}
+
+class ConsoleContentVisualizer implements ContentVisualizer{
+
+    private final ContentProvider contentProvider;
+
+    ConsoleContentVisualizer(ContentProvider contentProvider) {
+        this.contentProvider = requireNonNull(contentProvider);
+    }
+
+    @Override
+    public void listVisible(Path path) {
+        Set<Entry> entries = contentProvider.provideEntriesFrom(requireNonNull(path));
+        entries.stream()
+                .map(Entry::displayName)
+                .forEach(System.out::println);
     }
 }
